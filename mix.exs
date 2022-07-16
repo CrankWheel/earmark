02f5c85dd9beca21272e670a35d1254bde856a32
Efrén Fuentes <developer@efrenfuentes.net>
2022-07-16 17:23:25 -0400

    change Elixir requirements to support Elixir 1.10

--- mix.exs
+++ mix.exs
@@ -7,14 +7,14 @@
 
 
   @deps [
-    {:earmark_parser, "~> 1.4.26" },
+    {:earmark_parser, git: "https://github.com/CrankWheel/earmark_parser.git" },
     # {:earmark_parser, path: "../earmark_parser"},
     {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
     {:benchfella, "~> 0.3.0", only: [:dev]},
     {:earmark_ast_dsl, "~> 0.3.6", only: [:dev, :test]},
     {:excoveralls, "~> 0.14.4", only: [:test]},
-    # {:extractly, "~> 0.5.0", git: "https://github.com/RobertDober/extractly.git", tag: "v0.5.0-pre1", only: [:dev]},
-    {:extractly, "~> 0.5.3", only: [:dev]},
+    {:extractly, git: "https://github.com/CrankWheel/extractly.git", only: [:dev]},
+    # {:extractly, "~> 0.5.3", only: [:dev]},
     {:floki, "~> 0.21", only: [:dev, :test]},
     {:traverse, "~> 1.0.1", only: [:dev, :test]}
   ]
@@ -35,7 +35,7 @@
     [
       app: :earmark,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       escript: escript_config(),
       deps: @deps,
